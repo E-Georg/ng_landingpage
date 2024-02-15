1a508b1bf8dc74e74d0379112fa3101cd0585da9
GeorgStrassberger <georg.strassberger@googlemail.com>
2024-02-15 15:21:08 +0100

    updated click box witrh new style

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -10,4 +10,22 @@ import { FooterComponent } from '../footer/footer.component';
 })
 export class WelcomeComponent {
 
+  openAnalytics(){
+    const eanalytics: string = 'https://eanalytics.e-infra.com/';
+    this.openInNewTab(eanalytics);
+  }
+  openConsulting(){
+    const econsulting: string = 'https://servicehub.e-infra.com/nachhaltigkeit/';
+    this.openInNewTab(econsulting);
+  }
+
+  openInfra(){
+    const einfra: string= 'https://www.e-infra.com/';
+    this.openInNewTab(einfra);
+  }
+
+  openInNewTab(url: string):void{
+    window.open(url, '_blank');
+  }
+
 }
